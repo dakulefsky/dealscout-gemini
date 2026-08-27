d698b5d2a021a720f274f9702f2ea899ee26e16c
dakulefsky <dakulefsky@gmail.com>
2026-08-27 14:54:58 +0300

    Configure browser and Node globals for lint

--- eslint.config.js
+++ eslint.config.js
@@ -1,22 +1,35 @@
 import js from '@eslint/js';
+import globals from 'globals';
 import reactHooks from 'eslint-plugin-react-hooks';
 import reactRefresh from 'eslint-plugin-react-refresh';
 
 export default [
   { ignores: ['dist/**', 'node_modules/**', 'server/data/**'] },
-  js.configs.recommended,
   {
     files: ['src/**/*.{js,jsx}'],
+    ...js.configs.recommended,
+    languageOptions: { ...js.configs.recommended.languageOptions, globals: globals.browser },
     plugins: { 'react-hooks': reactHooks, 'react-refresh': reactRefresh },
     rules: {
+      ...js.configs.recommended.rules,
+      ...reactHooks.configs.recommended.rules,
       'no-unused-vars': ['warn', { argsIgnorePattern: '^_', varsIgnorePattern: '^_' }],
       'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
-      ...reactHooks.configs.recommended.rules,
     },
   },
   {
     files: ['server/**/*.js'],
-    languageOptions: { sourceType: 'commonjs', globals: { console: 'readonly', process: 'readonly', Buffer: 'readonly', __dirname: 'readonly', require: 'readonly', module: 'readonly' } },
-    rules: { 'no-unused-vars': ['warn', { argsIgnorePattern: '^_', varsIgnorePattern: '^_' }] },
+    ...js.configs.recommended,
+    languageOptions: { sourceType: 'commonjs', globals: globals.node },
+    rules: {
+      ...js.configs.recommended.rules,
+      'no-unused-vars': ['warn', { argsIgnorePattern: '^_', varsIgnorePattern: '^_' }],
+    },
+  },
+  {
+    files: ['server.js', '*.config.js'],
+    ...js.configs.recommended,
+    languageOptions: { sourceType: 'module', globals: globals.node },
+    rules: { ...js.configs.recommended.rules, 'no-unused-vars': ['warn', { argsIgnorePattern: '^_' }] },
   },
 ];
