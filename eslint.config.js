fc064646e4be0f22e27354f526cbc0704618b097
dakulefsky <dakulefsky@gmail.com>
2026-08-27 15:08:46 +0300

    Fix JSX lint parsing and focus CI on substantive errors

--- eslint.config.js
+++ eslint.config.js
@@ -3,33 +3,38 @@ import globals from 'globals';
 import reactHooks from 'eslint-plugin-react-hooks';
 import reactRefresh from 'eslint-plugin-react-refresh';
 
+const sharedRules = {
+  ...js.configs.recommended.rules,
+  'no-unused-vars': ['warn', { argsIgnorePattern: '^_', varsIgnorePattern: '^_' }],
+  'no-empty': ['warn', { allowEmptyCatch: true }],
+  'no-useless-escape': 'warn',
+};
+
 export default [
   { ignores: ['dist/**', 'node_modules/**', 'server/data/**'] },
   {
     files: ['src/**/*.{js,jsx}'],
-    ...js.configs.recommended,
-    languageOptions: { ...js.configs.recommended.languageOptions, globals: globals.browser },
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+      parserOptions: { ecmaFeatures: { jsx: true } },
+      globals: globals.browser,
+    },
     plugins: { 'react-hooks': reactHooks, 'react-refresh': reactRefresh },
     rules: {
-      ...js.configs.recommended.rules,
+      ...sharedRules,
       ...reactHooks.configs.recommended.rules,
-      'no-unused-vars': ['warn', { argsIgnorePattern: '^_', varsIgnorePattern: '^_' }],
       'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
     },
   },
   {
     files: ['server/**/*.js'],
-    ...js.configs.recommended,
-    languageOptions: { sourceType: 'commonjs', globals: globals.node },
-    rules: {
-      ...js.configs.recommended.rules,
-      'no-unused-vars': ['warn', { argsIgnorePattern: '^_', varsIgnorePattern: '^_' }],
-    },
+    languageOptions: { ecmaVersion: 'latest', sourceType: 'commonjs', globals: globals.node },
+    rules: sharedRules,
   },
   {
     files: ['server.js', '*.config.js'],
-    ...js.configs.recommended,
-    languageOptions: { sourceType: 'module', globals: globals.node },
-    rules: { ...js.configs.recommended.rules, 'no-unused-vars': ['warn', { argsIgnorePattern: '^_' }] },
+    languageOptions: { ecmaVersion: 'latest', sourceType: 'module', globals: globals.node },
+    rules: sharedRules,
   },
 ];
